688a9af84729f661a9fff3c14afeb70ab6dcf097
Josh Reisner <josh@joshreisner.com>
2015-11-23 23:17:16 -0800

    removing meta web app stuff

--- resources/views/templates/body.blade.php
+++ resources/views/templates/body.blade.php
@@ -7,8 +7,6 @@
 		<meta name="timezone" content="{!! Auth::user()->timezone !!}">
 		@endif
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-status-bar-style" content="black">
 		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/img/touch-icon.png">
 		<link rel="icon" href="/assets/img/favorite-icon.png">
 		{!! HTML::style('/assets/css/main.min.css') !!}
