8ba1de2aef990b718daa47846161c9ea6af932ee
Josh Reisner <josh@joshreisner.com>
2015-11-29 23:08:40 -0800

    improving invoice formatting

--- resources/views/project/invoice.blade.php
+++ resources/views/project/invoice.blade.php
@@ -2,34 +2,41 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<style type="text/css">
-			body { margin: 0; padding: 50pt 0 20pt; font-family: sans-serif; color: #444; }
-			#header { position: fixed; background-color: #eee; top: -34pt; right: -34pt; left: -34pt; padding: 20pt 34pt 0; }
-			#header h1 { font-weight: normal; }
-			#meta {  margin: 20pt 0 10pt; }
+			@font-face {
+			    font-family: 'AkkuratBold';
+			    src: url('{{ public_path() }}/assets/fonts/AkkBd_Pro_1.ttf');
+			}
+			@font-face {
+			    font-family: 'Akkurat';
+			    src: url('{{ public_path() }}/assets/fonts/AkkRg_Pro_1.ttf');
+			}
+			body { margin: 0; padding: 50pt 0 20pt; font-family: 'Akkurat'; color: #444; }
+			h1 { font-weight: normal; font-family: 'AkkuratBold'; }
+			#header { position: fixed; background-color: #eee; top: -34pt; right: -34pt; left: -34pt; padding: 35pt 34pt 15pt; }
+			#header h1 { font-size: 30pt; margin: 0; }
+			#meta {  margin: 28pt 0 10pt; }
 			#meta h1 { font-size: 20pt; margin: 0; }
-			#meta h1 span { color: #aaa; font-weight: normal; }
-			#meta h2 { color: #777; font-weight: normal; font-size: 14pt; margin: 0; }
+			#meta h1 span { color: #aaa; }
 			table { width: 100%; font-size: 10pt; }
 			table th, table td { padding: 6px 0; text-align: left; vertical-align: top; }
-			table th { border-bottom: 2pt solid #ccc; font-weight: normal; font-size: 10pt; color: #bbb; }
+			table th { border-bottom: 2pt solid #ddd; font-size: 10pt; font-weight: normal; }
+			table .date { white-space: nowrap; width: 15%; }
 			table .title { width: 55%; }
-			table .date { white-space: nowrap; text-align: right; width: 15%; }
 			table .amount { text-align: right; width: 15%; }
-			table .amount span { color: #999; margin-right: 15pt; }
 			table .hours { text-align: right; width: 15%; }
-			table tbody td { border-bottom: 1px solid #ccc; }
-			table tfoot td { font-weight: bold; font-size: 14pt; }
+			table tbody td { border-bottom: 1pt solid #eee; }
+			table tfoot td { font-size: 11pt; font-family: 'AkkuratBold'; }
 			#footer { 
 				color: #eee; position: fixed; background-color: #555;
 				bottom: -34pt; left: -34pt; right: -34pt; 
-				padding: 10pt 34pt 0 34pt; height: 40pt; 
-				font-size: 9.5pt; line-height: 1.4;
+				padding: 12pt 34pt 0 34pt; height: 44pt; 
+				font-size: 9.5pt; line-height: 1.2;
 			}
 			#footer a { color: inherit; text-decoration: none; }
 			#footer div { position: absolute; width: 240pt; }
 			#footer div:first-child { left: 34pt; }
 			#footer div:last-child { right: 34pt; text-align: right; }
-			#footer div span { color: #aaa; }
+			#footer div span { color: #aaa; width: 40pt; }
 		</style>
 	</head>
 	<body>
@@ -47,14 +54,13 @@
 			</div>
 		</div>
 		<div id="meta">
-			<h1>{{ $project->client->name }} <span>{{ $project->name }}</span></h1>
-			<h2>created {{ date('M j, Y') }}</h2>
+			<h1>{{ $project->client->name }} > <span>{{ $project->name }}</span></h1>
 		</div>
 		<table cellspacing="0">
 			<thead>
 				<tr>
-					<th>Task</th>
 					<th class="date">Date</th>
+					<th>Task</th>
 					@if (!$project->fixed)
 					<th class="hours">Hours</th>
 					<th class="amount">Amount</th>
@@ -64,8 +70,8 @@
 			<tbody>
 			@foreach ($project->tasks as $task)
 			<tr>
-				<td>{{ $task->title }}</td>
 				<td class="date">{{ $task->closed_at->format('M j, Y') }}</td>
+				<td>{{ $task->title }}</td>
 				@if (!$project->fixed)
 				<td class="hours">{{ format_number($task->hours) }}</td>
 				<td class="amount">@if ($task->amount > 0){{ $task->amount }}@else <span>~</span> @endif</td>
