59c9386140315f5e66a17140866f0bb354bf6c0e
Josh Reisner <josh@joshreisner.com>
2017-11-01 13:33:48 -0700

    Updating PDF issue for PHP 7.1

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,7 +6,6 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-//use Bugsnag\BugsnagLaravel\BugsnagExceptionHandler as ExceptionHandler;
 
 class Handler extends ExceptionHandler {
 
